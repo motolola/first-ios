bcd3c5f8910832ee58a3345034edd1c590f62554
Motolola <motolola@icloud.com>
2020-10-15 22:21:41 +0100

    first commit

--- Tax Calculator/ViewController.swift
+++ Tax Calculator/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBOutlet weak var price: UITextField!
+    
+    @IBOutlet weak var taxrate: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculate(_ sender: Any) {
+        
+        let priceField = Double(price.text!)!
+        let taxField = Double(taxrate.text!)!
+        
+        let total = priceField * taxField + priceField
+        
+        result.text = "$\(total)"
+        
+        
+    }
+    
 }
 
